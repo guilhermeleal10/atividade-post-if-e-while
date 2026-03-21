c859c18844c181b9d8c5b9013d0a42f9ad8b5b67
Guilherme Pantoja <guipantoja2022@hotmail.com>
2026-03-20 21:52:13 -0300

    questão3

---
+++ Questão3.php
@@ -0,0 +1,19 @@
+<form method="POST">
+    Número: <input type="number" name="n">
+    <input type="submit">
+</form>
+<?php
+if(isset($_POST["n"])){
+    $n = $_POST["n"];
+
+    $i = 1;
+    $soma = 0;
+
+    while($i <= $n){
+        $soma += $i;
+        $i++;
+    }
+
+    echo "Soma: " . $soma;
+}
+?>
